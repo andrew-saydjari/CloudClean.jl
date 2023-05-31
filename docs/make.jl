6bca2a96221afb0447fbc0378259b7083e96d545
Andrew Saydjari <saydjari@g.harvard.edu>
2023-05-30 22:26:11 -0400

    docs develop

---
+++ docs/make.jl
@@ -0,0 +1,24 @@
+using CloudClean
+using Documenter
+
+makedocs(
+    modules = [CloudClean],
+    clean=true,
+    highlightsig = true,
+    sitename= "CloudClean.jl",
+    format = Documenter.HTML(
+        # assets = ["assets/favicon.ico"],
+        prettyurls = get(ENV, "CI", nothing) == "true"
+    ),
+    pages    = [
+        # "Introduction" => "index.md",
+        "API Reference" => "api.md",
+        # "Contributing" => "contrib.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/andrew-saydjari/CloudClean.jl.git",
+    branch = "gh-pages",
+    devbranch = "main"
+)
